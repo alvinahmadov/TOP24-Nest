0bb6145ceb7fc339d7b82e40668ef33565a8ae92
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-11-08 22:23:38 +0400

    Fix date reformat
    Signed-off-by: Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>

--- src/api/pipes/transport.pipe.ts
+++ src/api/pipes/transport.pipe.ts
@@ -19,16 +19,13 @@ import {
 export class TransportCreatePipe
 	implements PipeTransform<any, ITransport> {
 	transform(data: any): ITransport {
-		const value = !env.api.compatMode ? data : transformToTransport(data);
+		const value: ITransport = !env.api.compatMode ? data : transformToTransport(data);
 		checkAndConvertBitrix(value, 'payload', 'transportPayload');
 		checkAndConvertBitrix(value, 'brand', 'transportBrand');
 		checkAndConvertBitrix(value, 'type', 'transportType');
 		checkAndConvertArrayBitrix(value, 'fixtures', 'fixtures', Reference.FIXTURES);
 		checkAndConvertArrayBitrix(value, 'riskClasses', 'riskClass', Reference.RISK_CLASSES);
-		if(env.api.compatMode)
-			reformatDateString<ITransportTransformer>(value, ['diag_date', 'osago_date']);
-		else
-			reformatDateString<ITransport>(value, ['diagnosticsDate', 'osagoExpiryDate']);
+		reformatDateString<ITransport>(value, ['diagnosticsDate', 'osagoExpiryDate']);
 
 		return value;
 	}
@@ -40,7 +37,7 @@ export class TransportUpdatePipe
 	transform(data: any): Partial<ITransport> {
 		delete data.createdAt;
 		delete data.updatedAt;
-		
+
 		const value = !env.api.compatMode ? data : transformToTransport(data);
 		checkAndConvertBitrix(value, 'payload', 'transportPayload');
 		checkAndConvertBitrix(value, 'brand', 'transportBrand');
@@ -51,7 +48,7 @@ export class TransportUpdatePipe
 			reformatDateString<ITransportTransformer>(value, ['diag_date', 'osago_date']);
 		else
 			reformatDateString<ITransport>(value, ['diagnosticsDate', 'osagoExpiryDate']);
-		
+
 		return value;
 	}
 }
