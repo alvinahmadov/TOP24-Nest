27e7999334403e098ecb3fc82a5b933612495f06
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-11-09 03:12:01 +0400

    Remove logging interceptor

--- src/api/middlewares/log.middleware.ts
+++ src/api/middlewares/log.middleware.ts
@@ -5,15 +5,9 @@ import {
 }                          from 'express';
 import {
 	Injectable,
-	CallHandler,
-	ExecutionContext,
 	NestMiddleware,
-	NestInterceptor,
 	Logger as NestLogger
 }                          from '@nestjs/common';
-import { Observable, tap } from 'rxjs';
-
-const millisToSec = (millis: number) => ((millis % 60000) / 1000).toFixed(0);
 
 @Injectable()
 export default class LoggerMiddleware
@@ -44,24 +38,5 @@ export default class LoggerMiddleware
 		});
 
 		next();
-	}
-}
-
-@Injectable()
-export class LoggingInterceptor
-	implements NestInterceptor {
-	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-		const now = Date.now();
-		return next
-			.handle()
-			.pipe(
-				tap(
-					() =>
-					{
-						const millis = Date.now() - now;
-						console.log(`Finished in ${millisToSec(millis)}.${millis} s`);
-					}
-				)
-			);
 	}
 }
