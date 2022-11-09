9c2e09a7215682381bf497a72220e9877d832700
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-11-09 16:26:50 +0400

    Add CompanyTransportFilterPipe

--- src/api/pipes/filter/index.ts
+++ src/api/pipes/filter/index.ts
@@ -1,15 +1,17 @@
-import AdminFilterPipe     from './admin-filter.pipe';
-import DriverFilterPipe    from './driver-filter.pipe';
+import AdminFilterPipe            from './admin-filter.pipe';
+import CompanyTransportFilterPipe from './company-filter.pipe';
+import DriverFilterPipe           from './driver-filter.pipe';
 import {
 	OfferFilterPipe,
 	OfferDriverFilterPipe,
 	OfferOrderFilterPipe
-}                          from './offer-filter.pipe';
-import OrderFilterPipe     from './order-filter.pipe';
-import TransportFilterPipe from './transport-filter.pipe';
+}                                 from './offer-filter.pipe';
+import OrderFilterPipe            from './order-filter.pipe';
+import TransportFilterPipe        from './transport-filter.pipe';
 
 export {
 	AdminFilterPipe,
+	CompanyTransportFilterPipe,
 	DriverFilterPipe,
 	OfferFilterPipe,
 	OfferDriverFilterPipe,
