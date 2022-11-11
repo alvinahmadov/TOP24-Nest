dc428b732cb7716e8a22f168a3421590a85a20b2
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-11-11 17:42:01 +0400

    Refactor route-info.decorator.ts

--- src/common/decorators/route-info.decorator.ts
+++ src/common/decorators/route-info.decorator.ts
@@ -1,6 +1,24 @@
-import { ApiOperation }      from '@nestjs/swagger';
-import { IApiRouteMetadata } from '@common/interfaces';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { IApiRouteMetadata }      from '@common/interfaces';
 
-/**@ignore*/
 export default (routeMetadata?: IApiRouteMetadata): MethodDecorator =>
-	ApiOperation(routeMetadata ? routeMetadata.api?.operation || {} : {});
+{
+	return (
+		target: Object,
+		propertyKey: string | symbol,
+		descriptor: TypedPropertyDescriptor<any>
+	) =>
+	{
+		if(routeMetadata?.api?.operation)
+			ApiOperation(routeMetadata.api.operation)(target, propertyKey, descriptor);
+
+		if(routeMetadata?.api?.queryOptions) {
+			const options = routeMetadata.api.queryOptions;
+
+			if(Array.isArray(options))
+				options.map(o => ApiQuery(o)(target, propertyKey, descriptor));
+			else ApiQuery(options)(target, propertyKey, descriptor);
+
+		}
+	};
+}
