5fff38824c879ec548a166a3ca23aabd1992794f
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-11-16 11:19:54 +0400

    Update offer filter dto

--- src/api/dto/offer/filter.dto.ts
+++ src/api/dto/offer/filter.dto.ts
@@ -2,8 +2,11 @@ import { PartialType }  from '@nestjs/swagger';
 import { InputType }    from '@nestjs/graphql';
 import { IOfferFilter } from '@common/interfaces';
 import OfferCreateDto   from './create.dto';
+import { OfferStatus }  from '@common/enums';
 
 @InputType()
 export default class OfferFilter
 	extends PartialType(OfferCreateDto)
-	implements IOfferFilter {}
+	implements IOfferFilter {
+	statuses?: OfferStatus[];
+}
