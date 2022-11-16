b1abd8f736b02ea6b875a5e2cfd6bf04647b04ee
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-11-16 15:36:11 +0400

    Add GatewayEvent entity

---
+++ src/models/gateway-event.entity.ts
@@ -0,0 +1,35 @@
+import { Table }         from 'sequelize-typescript';
+import { ObjectType }    from '@nestjs/graphql';
+import { TABLE_OPTIONS } from '@common/constants';
+import {
+	IGatewayEvent,
+	StringColumn,
+	JsonbColumn
+}                        from '@common/interfaces';
+import EntityModel       from './entity-model';
+
+/**
+ * Image model
+ *
+ * @class Image
+ * @interface IImage
+ * @extends EntityModel
+ * @description Image manipulation model for transport, driver and cargo
+ * */
+@ObjectType()
+@Table(TABLE_OPTIONS)
+export default class GatewayEvent
+	extends EntityModel<IGatewayEvent>
+	implements IGatewayEvent {
+	@StringColumn()
+	eventName?: 'cargo' | 'driver' | 'order' | string;
+
+	@StringColumn()
+	source?: string;
+
+	@StringColumn()
+	message?: string;
+
+	@JsonbColumn()
+	event?: any;
+}
