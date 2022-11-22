e37cb9034e86a80552c8ced0ca6566274e8a5efa
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-11-22 10:35:54 +0400

    Refactor repository method

--- src/repos/driver.repository.ts
+++ src/repos/driver.repository.ts
@@ -50,84 +50,80 @@ export default class DriverRepository
 		if(filter === null)
 			return [];
 
+		let {
+			from: offset = 0,
+			full = false,
+			count: limit
+		} = listFilter ?? {};
+
+		let {
+			term,
+			strict = true,
+			sortOrder = DEFAULT_SORT_ORDER,
+			orderStatus,
+			name,
+			patronymic,
+			lastName,
+			registrationAddress,
+			currentAddress,
+			address,
+			statuses,
+			payloadDate,
+			...rest
+		} = filter ?? {};
+
+		let hasTerm: boolean = term !== undefined && term !== '';
+
+		if(statuses && statuses.includes(1)) {
+			if(rest.isReady === undefined) {
+				rest.isReady = true;
+				statuses.push(0);
+			}
+		}
+
+		if(hasTerm) {
+			full = true;
+			strict = false;
+			statuses = statuses.filter((s: number) => s !== 4);
+		}
+		const conjunct: 'and' | 'or' | null = (strict === undefined || strict === true) ? 'and' : 'or';
+
 		return this.log(
-			async() =>
-			{
-				let {
-					from: offset = 0,
-					full = false,
-					count: limit
-				} = listFilter ?? {};
-				let {
-					term,
-					strict = true,
-					sortOrder = DEFAULT_SORT_ORDER,
-					orderStatus,
-					name,
-					patronymic,
-					lastName,
-					registrationAddress,
-					currentAddress,
-					address,
-					statuses,
-					payloadDate,
-					...rest
-				} = filter ?? {};
-
-				let hasTerm: boolean = term !== undefined && term !== '';
-
-				if(statuses && statuses.includes(1)) {
-					if(rest.isReady === undefined) {
-						rest.isReady = true;
-						statuses.push(0);
-					}
+			() => this.model.findAll(
+				{
+					where:   hasTerm
+					         ? this.whereClause()
+					               .eq('cargoId', rest?.cargoId)
+					               .eq('cargoinnId', rest?.cargoinnId)
+					               .lte('payloadDate', payloadDate)
+					               .eq('isReady', rest?.isReady)
+					               .in('status', statuses)
+						         .query
+					         : this.whereClause(conjunct)
+					               .in('phone', [rest?.phone, formatPhone(rest?.phone)])
+					               .iLike('name', name)
+					               .iLike('patronymic', patronymic)
+					               .iLike('lastName', lastName)
+					               .iLike('address', address)
+					               .iLike('registrationAddress', registrationAddress)
+					               .iLike('currentAddress', currentAddress)
+					               .lte('payloadDate', payloadDate)
+					               .in('status', statuses)
+					               .fromFilter(rest as IDriverFilter, 'eq')
+						         .query,
+					offset,
+					limit,
+					include: (!!full) ? [
+						{ model: CargoCompany },
+						{ model: CargoInnCompany },
+						{
+							model:   Transport,
+							include: [{ model: Image }]
+						},
+						{ model: Order }
+					] : []
 				}
-
-				if(hasTerm) {
-					full = true;
-					strict = false;
-					statuses = statuses.filter((s: number) => s !== 4);
-				}
-
-				const conjunct: 'and' | 'or' | null = (strict === undefined ||
-				                                       strict === true) ? 'and' : 'or';
-
-				return this.model.findAll(
-					{
-						where:   hasTerm
-						         ? this.whereClause()
-						               .eq('cargoId', rest?.cargoId)
-						               .eq('cargoinnId', rest?.cargoinnId)
-						               .lte('payloadDate', payloadDate)
-						               .eq('isReady', rest?.isReady)
-						               .in('status', statuses)
-							         .query
-						         : this.whereClause(conjunct)
-						               .in('phone', [rest?.phone, formatPhone(rest?.phone)])
-						               .iLike('name', name)
-						               .iLike('patronymic', patronymic)
-						               .iLike('lastName', lastName)
-						               .iLike('address', address)
-						               .iLike('registrationAddress', registrationAddress)
-						               .iLike('currentAddress', currentAddress)
-						               .lte('payloadDate', payloadDate)
-						               .in('status', statuses)
-						               .fromFilter(rest as IDriverFilter, 'eq')
-							         .query,
-						offset,
-						limit,
-						include: (!!full) ? [
-							{ model: CargoCompany },
-							{ model: CargoInnCompany },
-							{
-								model:   Transport,
-								include: [{ model: Image }]
-							},
-							{ model: Order }
-						] : []
-					}
-				);
-			},
+			),
 			{ id: 'getList' },
 			{
 				listFilter,
