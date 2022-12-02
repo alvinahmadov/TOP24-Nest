7ef953282a6b6cbf14f3097fe1cb63acb095ea82
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-12-02 16:57:49 +0400

    Update

--- install-pgcrypto.sh
+++ install-pgcrypto.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-create extension pgcrypto;
-EOSQL
+sudo -i -u postgres
+
+psql CREATE ROLE "$POSTGRES_USER" WITH LOGIN SUPERUSER PASSWORD "'$POSTGRES_PASSWORD'";
+psql CREATE EXTENSION pgcrypto;
+
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+#create extension pgcrypto;
+#EOSQL
