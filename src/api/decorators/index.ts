ca2291259e8d1cb9aa1ab8bda022501b00ab8ad2
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-12-03 15:02:36 +0400

    Refactor imports

--- src/api/decorators/index.ts
+++ src/api/decorators/index.ts
@@ -1,14 +1,10 @@
-import ApiFileBody      from './file.decorator';
-import ApiProperty      from './api-property.decorator';
+import NullableArgs     from './nullable-args.decorator';
 import ApiRoute         from './route.decorator';
 import ApiRouteInfo     from './route-info.decorator';
 import ApiRouteResponse from './route-response.decorator';
-import NullableArgs     from './nullable-args.decorator';
 import UserParam        from './user.decorator';
 
 export {
-	ApiFileBody,
-	ApiProperty,
 	ApiRoute,
 	ApiRouteInfo,
 	ApiRouteResponse,
