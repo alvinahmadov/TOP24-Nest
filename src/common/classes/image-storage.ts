a831b3768c8deda710f6ea8c0448ed35a8f40a41
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-12-10 18:35:12 +0400

    Refactor image object storage upload class

--- src/common/classes/image-storage.ts
+++ src/common/classes/image-storage.ts
@@ -1,16 +1,16 @@
-import fs                     from 'fs';
-import md5                    from 'md5';
-import path                   from 'path';
-import * as aws               from 'aws-sdk';
-import { lookup }             from 'mime-types';
-import { Logger }             from '@nestjs/common';
-import env                    from '@config/env';
-import { Bucket as BucketId } from '@common/constants';
+import fs         from 'fs';
+import md5        from 'md5';
+import path       from 'path';
+import * as aws   from 'aws-sdk';
+import { lookup } from 'mime-types';
+import { Logger } from '@nestjs/common';
+import env        from '@config/env';
+import { Bucket } from '@common/constants';
 import {
+	IAWSUploadResponse,
 	IBucketItem,
-	IObjectStorageParams,
-	IAWSUploadResponse
-}                             from '@common/interfaces';
+	IObjectStorageParams
+}                 from '@common/interfaces';
 
 /**
  * Extended file extension determination
@@ -45,11 +45,6 @@ export abstract class ObjectStorage {
 		this.logger = new Logger(ObjectStorage.name, { timestamp: true });
 	}
 
-	public setBucket(bucketId: string) {
-		this.params.bucketId = bucketId;
-		return this;
-	}
-
 	public abstract upload(file: IBucketItem): Promise<IAWSUploadResponse>;
 
 	public abstract remove(routeFullPath: string): Promise<boolean>;
@@ -75,7 +70,7 @@ export class LocalObjectStorage
 	}
 
 	public async upload(file: IBucketItem): Promise<IAWSUploadResponse> {
-		let bucket: string = this.params.bucketId || BucketId.COMMON_FOLDER,
+		let bucket: string = this.params.bucketId || Bucket.IMAGES_BUCKET,
 			fileBody: Buffer,
 			extension: string,
 			file_md5: string,
@@ -172,7 +167,7 @@ export class ExternalObjectStorage
 	extends ObjectStorage {
 	protected threads: any;
 	protected readonly s3: aws.S3;
-	protected readonly debug: boolean = false;
+	protected readonly debug: boolean = true;
 	protected readonly logger: Logger = new Logger(ExternalObjectStorage.name, { timestamp: true });
 	protected readonly ignoreList: string[] = [];
 
@@ -198,7 +193,7 @@ export class ExternalObjectStorage
 				connectTimeout: 10000
 			},
 			debug:        env.objectStorage.debug,
-			region:       env.yandex.cloud.region
+			region:       env.objectStorage.region
 		};
 
 		this.params = Object.assign(params, default_params);
@@ -277,72 +272,34 @@ export class ExternalObjectStorage
 	 * @returns {Promise<Object>} upload result
 	 */
 	public async upload(file: IBucketItem): Promise<IAWSUploadResponse> {
-		let bucket: string = this.params.bucketId || BucketId.COMMON_FOLDER,
-			fileBody: Buffer,
-			extension: string,
-			file_md5: string,
-			fileUploadName: string;
-
-		const debug_object = 'upload';
-
-		if(file.path) {
-			fileBody = fs.readFileSync(file.path);
-			extension = path.extname(file.path);
-			if(file.save_name)
-				fileUploadName = path.basename(file.path);
-			if(file.name)
-				fileUploadName = file.name;
-		}
-		else if(file.buffer) {
-			fileBody = file.buffer;
-			extension = `.${fileExt(file)}`;
-			if(file.name)
-				fileUploadName = file.name;
-		}
-		else
-			throw new Error('file.path or file.buffer must be provided!');
-
-		if(!fileUploadName) {
-			file_md5 = md5(fileBody);
-			fileUploadName = `${file_md5}${extension}`;
-		}
-
+		const params = this.prepare(file);
 		const s3 = this.s3;
-		const ContentType = lookup(fileUploadName) || 'image/jpeg';
-		const params = {
-			Bucket: bucket,
-			Key:    fileUploadName,
-			Body:   fileBody,
-			ContentType
-		};
 
+		console.log({ params });
 		if(this.debug)
-			this.logger.log('S3', debug_object, 'started');
-		if(this.debug)
-			this.logger.log('S3', debug_object, { params });
+			this.logObject('S3 Upload parameters:', params);
 
 		try {
 			const s3Promise = await new Promise<IAWSUploadResponse>(
 				(resolve, reject) =>
-					s3.upload(params, function(err: Error, data: any)
-					{
-						if(err) return reject(err);
-						return resolve(data);
-					})
+					s3.upload(
+						params,
+						(err: Error, data: IAWSUploadResponse) =>
+							err ? reject(err) : resolve(data)
+					)
 			);
 			if(this.debug)
-				this.logger.log('S3', debug_object, 'done:', s3Promise);
+				this.logger.log('S3 Upload success:', { s3Promise });
 			return s3Promise;
 		} catch(error) {
-			this.logger.error(error);
-			this.params.bucketId = null;
+			this.logger.error('S3 Upload failed: ', error.message);
 			throw error;
 		}
 	};
 
 	public async uploadMulti(files: IBucketItem[]): Promise<{ Location: string[] }> {
 		const uploadResponses = await Promise.all(
-			files.map(async file => this.upload(file))
+			files.map(async file => await this.upload(file))
 		);
 
 		return {
@@ -361,17 +318,13 @@ export class ExternalObjectStorage
 	 */
 	public async remove(routeFullPath: string): Promise<boolean> {
 		if(routeFullPath[0] == '/') routeFullPath = routeFullPath.slice(1);
-
 		const s3 = this.s3;
 		const Bucket = this.params.bucketId;
 		const Key = routeFullPath;
 		const params = { Bucket, Key };
 
-		const debug = this.debug;
-		const debugObject = 'deleteObject';
-		if(debug) {
-			this.logger.log('S3 started ', debugObject);
-			this.logger.log('S3', debugObject, params);
+		if(this.debug) {
+			this.logObject('S3 started deleteObject', params, routeFullPath);
 		}
 
 		try {
@@ -384,12 +337,55 @@ export class ExternalObjectStorage
 						return resolve(true);
 					})
 			);
-			if(debug)
-				this.logger.log(`S3 ${debugObject} done:`, s3Promise);
+			if(this.debug)
+				this.logger.log(`S3 deleteObject done:`, s3Promise);
 			return s3Promise;
 		} catch(error) {
-			if(debug) this.logger.error(`S3 ${debugObject} error:`, error);
+			if(this.debug) this.logger.error(`S3 deleteObject error:`, error.message);
 			return false;
 		}
+	}
+
+	private prepare(file: IBucketItem): aws.S3.Types.PutObjectRequest {
+		let bucket: string = this.params.bucketId || Bucket.IMAGES_BUCKET,
+			fileBody: Buffer,
+			extension: string,
+			fileNameMd5: string,
+			fileUploadName: string;
+
+		if(file.buffer) {
+			fileBody = file.buffer;
+			extension = `.${fileExt(file)}`;
+			if(file.name)
+				fileUploadName = file.name;
+		}
+		else if(file.path) {
+			fileBody = fs.readFileSync(file.path);
+			extension = path.extname(file.path);
+			if(file.save_name)
+				fileUploadName = path.basename(file.path);
+			if(file.name)
+				fileUploadName = file.name;
+		}
+		else
+			throw new Error('file.path or file.buffer must be provided!');
+
+		if(!fileUploadName) {
+			fileNameMd5 = md5(fileBody);
+			fileUploadName = `${fileNameMd5}${extension}`;
+		}
+
+		return {
+			Bucket:             bucket,
+			Key:                fileUploadName,
+			Body:               fileBody,
+			ContentType:        lookup(fileUploadName) || 'image/jpeg',
+			ContentDisposition: 'form-data; name="image";'
+		};
+	}
+
+	private logObject(message: string, objectRequest: aws.S3.Types.PutObjectRequest, ...args: any[]) {
+		const { Body, ...params } = objectRequest;
+		this.logger.debug(message, { params }, ...args);
 	}
 }
