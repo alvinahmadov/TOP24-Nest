a286d2ecf6b8aa81b8ba89b7c0acdaad614e1dc2
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-12-10 21:20:51 +0400

    Add bitrix key-value conversion to filter

--- src/api/pipes/filter/company-filter.pipe.ts
+++ src/api/pipes/filter/company-filter.pipe.ts
@@ -2,11 +2,26 @@ import { Injectable, PipeTransform }         from '@nestjs/common';
 import { ICompanyTransportFilter }           from '@common/interfaces';
 import env                                   from '@config/env';
 import { transformToCompanyTransportFilter } from '@common/utils/compat';
+import {
+	checkAndConvertArrayBitrix,
+	checkAndConvertBitrix
+}                                            from '@common/utils';
+import { Reference }                         from '@common/constants';
 
 @Injectable()
 export default class CompanyTransportFilterPipe
 	implements PipeTransform {
 	transform(data: any): ICompanyTransportFilter {
-		return !env.api.compatMode ? data : transformToCompanyTransportFilter(data);
+		let value: ICompanyTransportFilter = !env.api.compatMode ? data
+		                                                         : transformToCompanyTransportFilter(data);
+
+		checkAndConvertBitrix(value, 'payload', 'transportPayload');
+		checkAndConvertBitrix(value, 'brand', 'transportBrand');
+		checkAndConvertBitrix(value, 'model', 'transportModel');
+		checkAndConvertArrayBitrix(value, 'types', 'transportType', Reference.TRANSPORT_TYPES);
+		checkAndConvertArrayBitrix(value, 'fixtures', 'transportFixtures', Reference.FIXTURES);
+		checkAndConvertArrayBitrix(value, 'riskClasses', 'transportRiskClass', Reference.TRANSPORT_RISK_CLASSES);
+
+		return value;
 	}
 }
