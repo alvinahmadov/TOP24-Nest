d4591898868b7b170920ac52d5faeeb11b1c354d
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-12-12 07:05:47 +0400

    Update offer status from driver

--- src/api/services/offer.service.ts
+++ src/api/services/offer.service.ts
@@ -277,8 +277,7 @@ export default class OfferService
 			this.gateway.sendOrderEvent(eventObject, UserRole.LOGIST);
 		}
 
-		if(dto.status === OfferStatus.SENT &&
-		   dto.orderStatus === OrderStatus.ACCEPTED)
+		if(dto.orderStatus === OrderStatus.ACCEPTED)
 			dto.status = OfferStatus.RESPONDED;
 
 		const result = await this.repository.update(offer.id, dto);
