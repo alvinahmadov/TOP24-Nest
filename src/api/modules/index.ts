16486dd3af08e30c480c0369bcb2286a99b099cd
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-12-17 14:34:32 +0400

    Refactor events module

--- src/api/modules/index.ts
+++ src/api/modules/index.ts
@@ -1,12 +1,12 @@
-import AuthModule     from './auth.module';
-import EventsModule   from './events.module';
-import GQLModule      from './gql.module';
-import RESTModule     from './rest.module';
-import ServicesModule from './services.module';
+import AuthModule         from './auth.module';
+import GQLModule          from './gql.module';
+import NotificationModule from './notification.module';
+import RESTModule         from './rest.module';
+import ServicesModule     from './services.module';
 
 export {
 	AuthModule,
-	EventsModule,
+	NotificationModule,
 	GQLModule,
 	RESTModule,
 	ServicesModule
