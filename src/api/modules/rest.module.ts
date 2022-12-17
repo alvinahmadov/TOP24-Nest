1177989d7e5aaecc5b8b5c8ff379337a2919cede
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-12-17 14:36:49 +0400

    Remove events module from imports

--- src/api/modules/rest.module.ts
+++ src/api/modules/rest.module.ts
@@ -3,19 +3,14 @@ import {
 	MiddlewareConsumer,
 	NestModule
 }                           from '@nestjs/common';
-import { MulterModule }     from '@nestjs/platform-express';
 import CONTROLLERS          from '@api/controllers';
 import { LoggerMiddleware } from '@api/middlewares';
-import { memoryStorage }    from 'multer';
 import AuthModule           from './auth.module';
 import ServicesModule       from './services.module';
-import EventsModule         from './events.module';
 
 @Module({
 	        imports:     [
 		        AuthModule,
-		        EventsModule,
-		        MulterModule.register({ storage: memoryStorage() }),
 		        ServicesModule
 	        ],
 	        controllers: CONTROLLERS
