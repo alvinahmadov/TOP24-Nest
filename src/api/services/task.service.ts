0333f5dd8f471cc98eaed5fd5dc9db4e3a1dba05
AlvinAhmadov <alvin.dev.ahmadov@gmail.com>
2022-12-29 14:58:48 +0400

    Use log switcher

--- src/api/services/task.service.ts
+++ src/api/services/task.service.ts
@@ -48,6 +48,7 @@ export default class TaskService
 		protected readonly orderService: OrderService,
 		protected readonly notifications: NotificationGateway
 	) {
+		this.driverService.log = false;
 		this.orderService.log = false;
 	}
 
