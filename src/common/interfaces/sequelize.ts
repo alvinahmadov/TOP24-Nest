bbf40485973c029aa511652d08c852af14ef3570
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2023-01-11 18:08:54 +0400

    Fix column type

--- src/common/interfaces/sequelize.ts
+++ src/common/interfaces/sequelize.ts
@@ -72,6 +72,9 @@ export const IntArrayColumn = (options: TColumnOptions = {}): Function =>
 export const FloatColumn = (options: Omit<TColumnOptions, 'validate'> = {}): Function =>
 	MergeColumnOptions({ type: DataType.FLOAT, validate: { isFloat: true } }, options);
 
+export const RealArrayColumn = (options: Omit<TColumnOptions, 'validate'> = {}): Function =>
+	MergeColumnOptions({ type: DataType.ARRAY(DataType.FLOAT) }, options);
+
 export const BooleanColumn = (options: TColumnOptions = {}): Function =>
 	MergeColumnOptions({ type: DataType.BOOLEAN }, options);
 
