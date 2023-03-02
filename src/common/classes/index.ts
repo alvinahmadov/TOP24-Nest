b53b9920a0f9a2a5d8bda98f2ded982f30400685
AlvinAhmadov <alvin.dev.ahmadov@gmail.com>
2023-03-02 05:57:19 +0400

    Remove doc builder from common classes export

--- src/common/classes/index.ts
+++ src/common/classes/index.ts
@@ -1,6 +1,5 @@
 import { AxiosStatic, Axios }  from './axios';
 import ApiQuery                from './api-query';
-import DocumentTemplateBuilder from './template-builder';
 import EnvironmentParser       from './env-parser';
 import FieldTransformer        from './field-transformer';
 import WhereClause             from './where-clause';
@@ -11,7 +10,6 @@ export {
 	Axios,
 	AxiosStatic,
 	ApiQuery,
-	DocumentTemplateBuilder,
 	EnvironmentParser,
 	FieldTransformer,
 	WhereClause
